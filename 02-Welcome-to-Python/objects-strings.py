fe638c358f19ada24759f28b8d57cdf2feeb67c5
github username <github email address>
2020-03-26 21:18:22 -0700

    strings everywhere

---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,19 @@
+'In python everything is an object'
+'one type of object is string literals'
+'anything between quotes is interpreted as a string'
+
+'y3$ th4t m3ans these numbers and fancy symbols are string too'
+'but only when inside the quotes'
+
+"you can use double quotes"
+'or single quotes-- the important thing is to stay consistent'
+
+'you can even have empty strings, such as...'
+''
+
+"""
+You can even triple quote to create comment blocks
+"""
+
+'But hmm, what happens if you want your string to have quotations?'
+print('you\'re right, escape it')

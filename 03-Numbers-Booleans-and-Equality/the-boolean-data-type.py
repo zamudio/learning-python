67561b7f2853f3620bf3658473763db1516d47f5
github username <github email address>
2020-03-27 23:46:51 -0700

    boolean values

---
+++ 03-Numbers-Booleans-and-Equality/the-boolean-data-type.py
@@ -0,0 +1,22 @@
+# print(True)
+# print(False)
+
+if 2 + 2 == 3:
+    print(True)
+else:
+    print(False)
+
+# equality operators
+print(5 == 5)  # true
+print(5 == 8)  # false
+print(5 == '5')  # false
+print('ham' == 'ham')  # true
+print(5 == 5.0)  # true
+print(5 == 5.1)  # false
+
+print(True == True)  # true
+
+
+# inequality operators
+print(1 != 2)  # true
+print(1 != 1)  # false

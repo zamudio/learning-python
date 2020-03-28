e00cfaaf058a33d7dc547eb03059247d0934fa48
github username <github email address>
2020-03-27 23:35:55 -0700

    learnin basic expression syntax in python -- divison, modulo, etc

---
+++ 03-Numbers-Booleans-and-Equality/division-floor-division-and-the-modulo-operator.py
@@ -0,0 +1,7 @@
+print(15 / 3)  # 'regular' divison, returns as float
+print(14 / 3)  # example as to why division defaults to returning as a float
+
+print(14 // 3)  # floor division, chops off all decimals
+
+# modulo is rad... you divide as many times you can and return the remainder
+print(14 % 3)

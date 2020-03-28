c8569cf36f029c44d9dc7f5cd46dfcdd562c07e8
github username <github email address>
2020-03-27 23:26:35 -0700

    learnin basic expression syntax in python

---
+++ 03-Numbers-Booleans-and-Equality/mathematical-expressions.py
@@ -0,0 +1,11 @@
+print(3 + 4)  # printing an expression that adds two ints
+print(3.5 + 6.3)  # printing an expression that adds two floats
+print(10 - 5)  # subtraction
+print(5 * 3)  # multiplication
+
+print('ba' + 'con')  # concatenation
+print('lol ' * 32)  # prints string 32 times
+
+print(10 + 3.8)  # will return float value
+
+print(5 ** 3)  # exponentiation

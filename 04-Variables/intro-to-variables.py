af047a51de5a2fce69c7462e8bf099f12c993c24
github username <github email address>
2020-03-29 02:10:36 -0700

    variable intro

---
+++ 04-Variables/intro-to-variables.py
@@ -0,0 +1,9 @@
+# variable is a name assigned to an object
+
+name = 'Big Chungus'
+age = 666
+occupation = 'Hellraiser'
+
+print(f'My name is {name}, age {age}, and I\'m a {occupation}')
+
+print('Mein named ist', name, age, 'jahre alt, und ich bin', occupation)

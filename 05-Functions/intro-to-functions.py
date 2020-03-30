651314e6112be18bd80efd641288bc14ffcd6637
github username <github email address>
2020-03-29 23:23:13 -0700

    Intro into function syntax

---
+++ 05-Functions/intro-to-functions.py
@@ -0,0 +1,21 @@
+# a function is a collection of 1 or more statements
+def function_name():
+    # statements
+    print('Hello World')
+
+# unlike some other languages, no end needed on the function in python
+
+
+def function_name_two(arguement):
+    if arguement == 3:
+        print('Yes')
+    else:
+        print('No')
+
+
+function_name()  # how to call function
+
+a = 1
+b = 2
+c = a + b
+function_name_two(c)

0d117dd5fe9df8c127d56a303b2d388fd11d3229
github username <github email address>
2020-04-04 19:07:01 -0700

    str slicing

---
+++ 05-Functions/return-values.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+# parantheses not needed for return value
+
+
+print(add(3, 5))
+
+
+def z():
+    return 100
+
+
+print(z())
---
+++ 06-Strings-The-Basics/string-slicing-II-slicing-by-steps.py
@@ -0,0 +1,4 @@
+a = 'abcdefghijklmnopqrstuvwxyz'
+
+print(a[::-1])
+print(a[4:24:2])

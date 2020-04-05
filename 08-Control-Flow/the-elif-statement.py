6b00e23e5bf277b6c678cbf594ced90017f7d9f3
github username <github email address>
2020-04-04 19:36:08 -0700

    if-elif-else

---
+++ 08-Control-Flow/the-elif-statement.py
@@ -0,0 +1,12 @@
+def pos_or_neg(num):
+    if num > 0:
+        return 'Positive num bro'
+    elif num < 0:
+        return 'Negative num, oof'
+    else:
+        return 'Zeeeeroooo'
+
+
+print(pos_or_neg(1))
+print(pos_or_neg(-1))
+print(pos_or_neg(0))

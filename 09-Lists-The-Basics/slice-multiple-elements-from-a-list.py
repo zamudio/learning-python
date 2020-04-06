138427449781bf1f7050735fa0a74f73ce86dc16
github username <github email address>
2020-04-05 22:18:47 -0700

    list syntax

---
+++ 09-Lists-The-Basics/slice-multiple-elements-from-a-list.py
@@ -0,0 +1,10 @@
+pizza_toppings = ['pepperoni', 'sausage',
+                  'mushrooms', 'peppers', 'onions', 'pineapple']
+
+pizza_toppings[0:2]    # Pep, Sausage
+pizza_toppings[:2]     # Pep, Sausage
+
+pizza_toppings[4:5]    # onions
+pizza_toppings[4:]     # onions and pineapple
+
+pizza_toppings[::2]    # pep, mushrooms, onions
---
+++ 09-Lists-The-Basics/the-in-and-not-in-operators-on-a-list.py
@@ -0,0 +1,4 @@
+meals = ['bfast', 'lunch', 'din']
+print('lunch' in meals)    # true
+print('LUNCH' in meals)    # false
+print('dINnEr' not in meals)    # true
